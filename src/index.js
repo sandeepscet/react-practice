23c24d87e8a1350a32993ac97ae740fc27d348fe
sandip Kachhadiya <sandeep.scet@gmail.com>
2023-01-28 17:47:35 +0530

    united STATE

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import  Component  from './Component';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+class App extends React.Component {
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Component />
-    <App />
-  </React.StrictMode>
-);
+  state = {
+    count: 0,
+  }
+  render() {
+    const count = this.state.count
+    return (
+      <div className='App'>
+        <h1>{count} </h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        <div>
+          <button
+            onClick={() => this.setState({ count: this.state.count + 1 })}
+          >
+            Add One
+          </button>{' '}
+          <button
+            onClick={() => this.setState({ count: this.state.count - 1 })}
+          >
+            Minus One
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
