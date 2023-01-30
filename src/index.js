6078d088bedbb32d53994bb313373beec0d7477e
sandip Kachhadiya <sandeep.scet@gmail.com>
2023-01-30 11:59:47 +0530

    route 53

--- src/index.js
+++ src/index.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route,Outlet, Link } from "react-router-dom";
 
-// if it is functional components
-class App extends Component {
-  greetPeople = (e) => {
-    console.log(e);
-    alert('welcome to the Jungle!!')
-  }
-  render() {
-    return <button onClick={this.greetPeople}> Yo </button>
-  }
+// Home component
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+const Home = (props) => <h1>Welcome Home</h1>
+// About component
+const About = (props) => <h1>About Us</h1>
+// Contact component
+const Contact = (props) => <h1>Contact us</h1>
+// Challenge component
+const Challenges = (props) => (
+  <div>
+    <h1>30 Days Of React Challenge</h1>
+  </div>
+)
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout  />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<Challenges />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
